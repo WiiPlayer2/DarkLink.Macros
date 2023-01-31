f7a1b28471f1fb1b98c5993d2d4e81e573649e94
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-01-31 06:26:14 +0100

    Use invariant culture

--- DarkLink.Macros/DarkLink.Macros/Generator.cs
+++ DarkLink.Macros/DarkLink.Macros/Generator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -47,7 +48,7 @@ public class Generator : IIncrementalGenerator
 
             foreach (var arguments in definition.ArgumentSets)
             {
-                var generated = string.Format(definition.Template, arguments);
+                var generated = string.Format(CultureInfo.InvariantCulture, definition.Template, arguments);
                 codeBuilder.WriteLine(generated);
             }
 
